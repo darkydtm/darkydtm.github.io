fbfbddd02f7de9562d0fb02a05342fcc39234289
DarkyDTM <thedarkygd@gmail.con>
2026-03-28 21:11:09 +0500

    Added protection against dark reader. It interfered with the loading of the bg

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
-function copyText(text) {
-    var dummyInput = document.createElement("textarea");
-    document.body.appendChild(dummyInput);
-    dummyInput.value = text;
-    dummyInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummyInput);
+const warning = document.querySelector('.dr-warning');
+
+function checkDarkReader() {
+	if (document.documentElement.hasAttribute('data-darkreader-mode')) {
+		warning.classList.add('visible');
+	} else {
+		warning.classList.remove('visible');
+	}
 }
 
-function playSound(path) {
-  var audio = new Audio(path);
-  audio.play();
-}
+checkDarkReader();
 
-function daysToTargetDate(targetMonth, targetDay) {
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-  const targetDate = new Date(currentYear, targetMonth - 1, targetDay);
-  if (targetDate < currentDate) {
-    targetDate.setFullYear(currentYear + 1);
-  }
-  const targetDateMS = targetDate.getTime();
-  const currentDateMS = currentDate.getTime();
-  const timeDifferenceMS = targetDateMS - currentDateMS;
-  const days = Math.floor(timeDifferenceMS / (1000 * 60 * 60 * 24));
-  return days;
-}
+const observer = new MutationObserver(checkDarkReader);
 
-document.addEventListener('DOMContentLoaded', function() {
-    const toHB_element = document.getElementById("toHB");
-    toHB_element.innerHTML = "До дня рождения " + daysToTargetDate(10, 2) + " дней";
+observer.observe(document.documentElement, {
+	attributes: true,
+	attributeFilter: ['data-darkreader-mode']
 });
